a41965f6b8d73b5bd5c12fef8e82a627cba84306
A1caida <46443450+A1caida@users.noreply.github.com>
2021-09-26 15:38:25 +0400

    hotfix

--- html_parser/Program.cs
+++ html_parser/Program.cs
@@ -15,7 +15,7 @@ namespace html_parser
             db_work a = new db_work(); ;
 
             Console.Write("Путь к файлу: ");
-            text = Console.ReadLine();//C:\Users\A1caida\Desktop\file1.html
+            text = Console.ReadLine();
             FileInfo path = new System.IO.FileInfo(text);
 
             extension = Path.GetExtension(text);
@@ -26,7 +26,7 @@ namespace html_parser
                         html html = new html();
                         words = html.Count(text);
                         html.Show(words);
-                        //a.insert(words);
+                        a.insert(words);
                         Console.ReadKey();
                         return;
                     }
